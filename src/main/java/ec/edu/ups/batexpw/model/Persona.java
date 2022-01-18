6d9d08e24afdd06ae90207e30d8d2c3adc72dc52
SegundoRolando <scallet@est.ups.edu.ec>
2022-01-18 02:18:33 -0500

    Creación de la clase persona

---
+++ src/main/java/ec/edu/ups/batexpw/model/Persona.java
@@ -0,0 +1,77 @@
+package ec.edu.ups.batexpw.model;
+
+import java.util.Date;
+
+public class Persona {
+	private int id;
+	private String cedula;
+	private String nombre;
+	private String apellido;
+	private String direccion;
+	private String telefono;
+	private String email;
+	private Date fechaN;
+	private String genero;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getCedula() {
+		return cedula;
+	}
+	public void setCedula(String cedula) {
+		this.cedula = cedula;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	public String getDireccion() {
+		return direccion;
+	}
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+	public String getTelefono() {
+		return telefono;
+	}
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Date getFechaN() {
+		return fechaN;
+	}
+	public void setFechaN(Date fechaN) {
+		this.fechaN = fechaN;
+	}
+	public String getGenero() {
+		return genero;
+	}
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+	@Override
+	public String toString() {
+		return "RolPersona [id=" + id + ", cedula=" + cedula + ", nombre=" + nombre + ", apellido=" + apellido
+				+ ", direccion=" + direccion + ", telefono=" + telefono + ", email=" + email + ", fechaN=" + fechaN
+				+ ", genero=" + genero + "]";
+	}
+	
+}
